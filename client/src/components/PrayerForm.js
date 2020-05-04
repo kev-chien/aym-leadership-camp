6d1579b1eb7989f976786ded5e7334a2f43cca7d
Mike Curry <michael.curry@buildingconnected.com>
2020-05-04 13:11:22 -0700

    lint, convert to theme colors

--- client/src/components/PrayerForm.js
+++ client/src/components/PrayerForm.js
@@ -1,122 +1,120 @@
-import React from 'react'
-import styled from 'styled-components'
-import usePrayerForm from '../CustomHook'
+import React from "react";
+import styled from "styled-components";
+import usePrayerForm from "../CustomHook";
 
-const StyledContainer = styled.div` 
-    display: flex;
-    flex-direction: flex-column;
-    justify-content: space-between;
-    width: 388px;
-    position: relative;
-    height: 392px;
-    left: 735px;
-    top: 310px;
-    background: #FFFFFF;
-    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
-    order-radius: 10px;
+import theme from './theme'
 
-    form {
-        position: absolute;
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: flex-column;
+  justify-content: space-between;
+  margin-top: 30px;
+  width: 388px;
+  height: 392px;
+  background: ${theme.colors.white};
+  box-shadow: 0px 0px 20px ${theme.colors.shadow};
+  border-radius: 10px;
 
-        h3 {
-            width: 329px;
-            height: 26px;
-            left: 764px;
-            top: 341px;
-            font-family: Manrope;
-            font-style: normal;
-            font-weight: 600;
-            font-size: 20px;
-            line-height: 26px;
-            text-transform: uppercase;
-            color: #544244;
-        }
+  form {
+    position: absolute;
 
-        input[ type=text ] {
-            width: 328px;
-            height: 41px;
-            left: 765px;
-            top: 377px;
-            background: #FFFFFF;
-            border: 1px solid #E5E5E5;
-            box-sizing: border - box;
-            border-radius: 5px;
-            ::placeholder {
-                font-family: Manrope;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 16px;
-                line-height: 21px;
-                color: #C1C1C1;
-            }
-        }
-
-        input[ type=textarea ] {
-            width: 328px;
-            height: 185px;
-            left: 765px;
-            top: 428px;
-            background: #FFFFFF;
-            border: 1px solid #E5E5E5;
-            box-sizing: border - box;
-            border-radius: 5px;
-            ::placeholder {
-                font-family: Manrope;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 16px;
-                line-height: 21px;
-                color: #C1C1C1;
-            }
-        }   
-
-        button {
-            width: 28px;
-            height: 39px;
-            left: 765px;
-            top: 633px;
-            background: #578982;
-            border-radius: 5px;
-        }
+    h3 {
+      width: 329px;
+      height: 26px;
+      left: 764px;
+      top: 341px;
+      font-family: Manrope;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 20px;
+      line-height: 26px;
+      text-transform: uppercase;
+      color: ${theme.colors.burgundy};
     }
+
+    input[type="text"] {
+      width: 328px;
+      height: 41px;
+      left: 765px;
+      top: 377px;
+      background: ${theme.colors.white};
+      border: 1px solid ${theme.colors.background};
+      box-sizing: border - box;
+      border-radius: 5px;
+      ::placeholder {
+        font-family: Manrope;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 21px;
+        color: ${theme.colors.placeholder};
+      }
+    }
+
+    input[type="textarea"] {
+      width: 328px;
+      height: 185px;
+      left: 765px;
+      top: 428px;
+      background: ${theme.colors.white};
+      border: 1px solid ${theme.colors.background};
+      box-sizing: border - box;
+      border-radius: 5px;
+      ::placeholder {
+        font-family: Manrope;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 21px;
+        color: ${theme.colors.placeholder};
+      }
+    }
+
+    button {
+      width: 28px;
+      height: 39px;
+      left: 765px;
+      top: 633px;
+      background: #578982;
+      border-radius: 5px;
+    }
+  }
 `;
 
 const PrayerForm = () => {
-    //need to figure out this will communicate with server
-    const submit = () => {
-        alert(`Prayer submitted!
-               Name: ${ inputs.name}
-               Accomplishment: ${ inputs.accomplishment} `);
-    }
+  //need to figure out this will communicate with server
+  const submit = () => {
+    alert(`Prayer submitted!
+               Name: ${inputs.name}
+               Accomplishment: ${inputs.accomplishment} `);
+  };
 
-    const { inputs, handleInputChange, handleSubmit } = usePrayerForm(submit);
+  const { inputs, handleInputChange, handleSubmit } = usePrayerForm(submit);
 
-    return (
-        <StyledContainer>
-            <h3>
-                Share What You Accomplished
-            </h3>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    onChange={handleInputChange}
-                    value={inputs.name}
-                    required
-                />
-                <input
-                    type="textarea"
-                    name="accomplishment"
-                    placeholder="What did you do?"
-                    onChange={handleInputChange}
-                    value={inputs.accomplishment}
-                    required
-                />
-                <button type="submit" />
-            </form>
-        </StyledContainer >
-    )
-}
+  return (
+    <StyledContainer>
+      <h3>Share What You Accomplished</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleInputChange}
+          value={inputs.name}
+          required
+        />
+        <input
+          type="textarea"
+          name="accomplishment"
+          placeholder="What did you do?"
+          onChange={handleInputChange}
+          value={inputs.accomplishment}
+          required
+        />
+        <button type="submit" />
+      </form>
+    </StyledContainer>
+  );
+};
 
-export default PrayerForm
+export default PrayerForm;
--- client/src/components/theme.js
+++ client/src/components/theme.js
@@ -1,5 +1,6 @@
 export default {
   colors: {
+    placeholder: "#C1C1C1",
     fadedPink: "#BBA6A8",
     pink: "#A17379",
     burgundy: "#544244",
@@ -7,9 +8,10 @@ export default {
     darkGreen: "#265750",
     background: "#E5E5E5",
     white: "#FFF",
+    shadow: "rgba(0, 0, 0, 0.25)",
   },
   breakpoints: {
     sm: 500,
-    md: 725
-  }
+    md: 725,
+  },
 };
